f7c57604a8d88616deb2fc0a442175b2f4c46bec
Piyush Aanand <aanandpiyush1507@gmail.com>
2024-08-30 00:45:33 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ConverterForm from './components/converterForm'
+
+function App() {
+  return (
+    <div className='currency-converter'>
+      <h2 className="converter-title">Currency Convertor</h2>
+     <ConverterForm/>
+    </div>
+     
+  )
+}
+
+export default App
